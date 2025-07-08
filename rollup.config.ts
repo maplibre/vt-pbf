c9977376e1c264ef5cdd8c298a0bc4d5f78ed7bf
mwilsnd <mwilsnd@gmail.com>
2025-07-08 19:09:55 -0400

    Add rollup

---
+++ rollup.config.ts
@@ -0,0 +1,37 @@
+import {type RollupOptions} from 'rollup';
+import strip from '@rollup/plugin-strip';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+const config: RollupOptions = {
+    input: ['index.ts'],
+    output: [
+        {
+            file: "dist/index.es.js",
+            format: 'es',
+            sourcemap: true
+        }
+    ],
+    onwarn: (message) => {
+        console.error(message);
+        throw message;
+    },
+    plugins: [
+        strip({
+            sourceMap: true
+        }),
+        terser({
+            compress: {
+                pure_getters: true,
+                passes: 3
+            },
+            sourceMap: true
+        }),
+        typescript(),
+        nodeResolve()
+    ],
+    external: [/node_modules/]
+};
+
+export default config;
